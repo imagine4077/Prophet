0b5b414bda66bb531a34407ae892a117e6fdf80c
imagine4077 <imagine4077@gmail.com>
2018-09-04 22:35:29 +0800

    update several html filter utilities

---
+++ Prophet/src/main/java/com/hackathon/prophet/test/Text4Test.java
@@ -0,0 +1,16 @@
+package com.hackathon.prophet.test;
+
+public class Text4Test
+{
+    public static String text0 = "";
+
+    public static String text1 = "";
+
+    public static String text2 = "";
+
+    public static String text3 = "";
+
+    public static String text4 = "";
+
+    public static String text5 = "";
+}
