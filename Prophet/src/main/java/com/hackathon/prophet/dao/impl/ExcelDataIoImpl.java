90fd019268f88c975b56cad47bbbcc4d6304b1d1
imagine4077 <imagine4077@gmail.com>
2018-09-12 08:25:30 +0800

    excell data io

--- Prophet/src/main/java/com/hackathon/prophet/dao/impl/ExcelDataIoImpl.java
+++ Prophet/src/main/java/com/hackathon/prophet/dao/impl/ExcelDataIoImpl.java
@@ -2,18 +2,34 @@ package com.hackathon.prophet.dao.impl;
 
 import com.hackathon.prophet.dao.DataIO;
 import com.hackathon.prophet.dao.DataObject;
+import com.hackathon.prophet.pojo.SingleDtsBase;
+import com.hackathon.prophet.utils.ExcelUtils;
+
+import java.io.File;
+import java.util.List;
 
 public class ExcelDataIoImpl implements DataIO
 {
     @Override
     public DataObject readTrainSet()
     {
-        return null;
+        File train = new File(ExcelDataIoImpl.class.getClassLoader().getResource("train.xlsx").getPath());
+        String xml = "/TrainMapper.xml";
+        List<SingleDtsBase> dtses = ExcelUtils.parse(train, SingleDtsBase.class, xml);
+        return new DataObject<SingleDtsBase>(dtses);
     }
 
     @Override
     public DataObject readTestSet()
     {
-        return null;
+        File train = new File(ExcelDataIoImpl.class.getClassLoader().getResource("test.xlsx").getPath());
+        String xml = "/TestMapper.xml";
+        List<SingleDtsBase> dtses = ExcelUtils.parse(train, SingleDtsBase.class, xml);
+        return new DataObject<SingleDtsBase>(dtses);
+    }
+
+    public boolean appendWrite()
+    {
+        return true;
     }
 }
